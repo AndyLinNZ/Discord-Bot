b96d7d0bcf01418973b60afe9ac45cbb94f2e6c0
SilverCrocus <2muchhivinlogic@gmail.com>
2020-02-25 21:13:32 +1300

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,54 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "now ")
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined a server.")
+
+@client.event
+async def on_member_remove(member):
+    print(f"{member} has left a server.")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command()
+async def gay(ctx):
+    await ctx.send(f"Lmao you're a fag!")
+
+@client.command(aliases=["8ball"])
+async def _8ball(ctx, *, question):
+    responses = ["It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+print("hello")
+
+
+client.run("NjgxNjgzNjA1ODIyNzY3MTg3.XlSICA.6CEl1PBSi1p-7SdCWm8Y340K9E8")
